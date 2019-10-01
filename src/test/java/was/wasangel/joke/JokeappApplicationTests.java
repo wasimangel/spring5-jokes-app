4cd16b26c4956e87d61d0f7a51546cfc38c2e0c9
Wasim <wasims831@gmail.com>
2019-10-01 16:45:32 +0530

    first commit

---
+++ src/test/java/was/wasangel/joke/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package was.wasangel.joke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
